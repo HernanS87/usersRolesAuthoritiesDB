62628b25382aab81221339226a25b33cfb7faae4
Hernán Santarelli <hernansantarelli87@gmail.com>
2025-02-22 19:50:29 -0300

    add new end-points

--- src/main/java/com/example/usersRolesAuthoritiesDB/controller/HelloWorldController.java
+++ src/main/java/com/example/usersRolesAuthoritiesDB/controller/HelloWorldController.java
@@ -1,19 +1,43 @@
 package com.example.usersRolesAuthoritiesDB.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
+@Slf4j // esta anotación me permite loggear mensajes
 @RestController
 @RequestMapping("hello/")
 @PreAuthorize("denyAll()")
 public class HelloWorldController {
 
-    @GetMapping("security")
+    @GetMapping("security1")
     @PreAuthorize("hasAuthority('READ')")
-    public String helloWidthSec(){
-        return "Hello World width SECURITY";
+    public String helloWidthSec(Principal principal){
+        log.info("Hola amigo 1: {}", principal.getName());
+        return "Hello World width SECURITY 1 " + principal.getName();
+    }
+
+    @GetMapping("security2")
+    @PreAuthorize("hasAuthority('READ')")
+    public String helloWidthSec2(@AuthenticationPrincipal User user){
+        log.info("Hola amigo 2: {}", user.getUsername());
+        return "Hello World width SECURITY 2 " + user.getUsername();
+    }
+
+    @GetMapping("security3")
+    @PreAuthorize("hasAuthority('READ')")
+    public String helloWidthSec3(Principal principal){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info("Hola amigo 3: {}", authentication.getName());
+        return "Hello World width SECURITY 3 " + authentication.getName();
     }
 
     @GetMapping("free")
